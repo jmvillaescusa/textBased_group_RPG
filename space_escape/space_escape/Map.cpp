e748ca5c7aab3639693add227244fa7fec009c41
jmvillaescusa <jmvillaescusa99@gmail.com>
2019-04-04 16:52:06 -0400

    Display Map

---
+++ space_escape/space_escape/Map.cpp
@@ -0,0 +1,34 @@
+#include "Map.h"
+
+Map::Map() {
+	
+}; Map::~Map() {};
+
+void Map::DislayMap() {
+	char temp;
+	for (int i = 0; i < 11; i++) {
+		for (int j = 0; j < 20; j++) {
+			temp = MapIndex(area1_2[i][j]);
+			std::cout << temp;
+		}
+		std::cout << std::endl;
+	}
+};
+char Map::MapIndex(int temp) {
+	switch (temp) {
+	case 0:
+		return NOTHING;
+	case 1:
+		return WALL;
+	case 2:
+		return SPACE;
+	case 3:
+		return AVATAR;
+	case 4:
+		return NPC;
+	case 5:
+		return DOOR;
+	default:
+		NULL;
+	}
+};
--- space_escape/space_escape/space_escape.cpp
+++ space_escape/space_escape/space_escape.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "Map.h"
 
 int main()
 {
+	Map m;
+	m.DislayMap();
 	return 0;
 }
