bda42e6f9fff919b81c0f888b1e7bd119404861a
jmvillaescusa <jmvillaescusa99@gmail.com>
2019-04-05 16:21:17 -0400

    Inherited Player/Enemy to BaseStatsClass

--- space_escape/space_escape/Enemy.h
+++ space_escape/space_escape/Enemy.h
@@ -5,31 +5,19 @@
 
 #include"Basestat.h"
 
-class EnemyClass {
-private:
-	unsigned int health;
-	int speed;
-	int power;
-	int luck;
-	int deffence;
-
+class EnemyClass : public BaseStatClass {
 public:
-
 	int getHealth() { return health; }
 	void setHealth(unsigned int e_health) { health = e_health; }
 
 	int getPower() { return power; }
 	void setPower(int e_power) { power = e_power; }
 
-	int getDeffence() { return deffence; }
-	void setDeffence(int e_deffence) { deffence = e_deffence; }
+	int getDefense() { return defense; }
+	void setDefense(int e_defense) { defense = e_defense; }
 
 	int getSpeed() { return speed; }
 	void setSpeed(int e_speed) { speed = e_speed; }
-
-	int getLuck() { return luck; }
-	void setLuck(int e_luck) { luck = e_luck; }
-
 };
 
 #endif // !1
--- space_escape/space_escape/Player.h
+++ space_escape/space_escape/Player.h
@@ -1,21 +1,14 @@
 #pragma once
 #include <time.h>
+#include "Basestat.h"
 
-class Player {
+class Player : public BaseStatClass {
 private:
-	int Health;
-	int Defense;
-	int Attack;
-	int Speed;
+	int luck;
 
 public:
-	
-
-	int setHealth(int h) { Health = h; }
-	int setDefence(int d) { Defense = d; }
-	int setAttack(int a) { Attack = a; }
-	int setSpeed(int s) { Speed = s; }
-
+	int getLuck() { return luck; }
+	void setLuck(int m_luck) { luck = m_luck; }
 
 	void Update();
 	Player();
