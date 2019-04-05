715bee0ba0263108822324bc971c618654c84f5d
Dylanramper <44001627+Dylanramper@users.noreply.github.com>
2019-04-05 15:09:49 -0400

    something

--- space_escape/space_escape/space_escape.cpp
+++ space_escape/space_escape/space_escape.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include "Map.h"
+#include "Player.h"
+#include <conio.h>
+
+//Controls for player movement
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
 
 int main()
 {
+	Player player;
+	bool isRunning = true;
 	Map m;
 	m.DislayMap();
+	int c = 0;
+	int playerPosX = 4;
+	int playerPosY = 1;
+
+	while (isRunning) {
+		player.Update();
+
+		//Player Movement
+	}
+
 	return 0;
 }
