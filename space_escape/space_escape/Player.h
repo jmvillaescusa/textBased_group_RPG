bab2d71f3cf57d84da5f02bc01489e3745e78bba
jmvillaescusa <jmvillaescusa99@gmail.com>
2019-04-05 15:38:50 -0400

    s

--- space_escape/space_escape/Player.h
+++ space_escape/space_escape/Player.h
@@ -3,9 +3,20 @@
 
 class Player {
 private:
-	
+	int Health;
+	int Defense;
+	int Attack;
+	int Speed;
 
 public:
+	
+
+	int setHealth(int h) { Health = h; }
+	int setDefence(int d) { Defense = d; }
+	int setAttack(int a) { Attack = a; }
+	int setSpeed(int s) { Speed = s; }
+
+
 	void Update();
 	Player();
 	~Player();
