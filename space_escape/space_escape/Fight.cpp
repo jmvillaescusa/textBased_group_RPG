da980aa1e278f5167ac8471a263751d84ce4be78
jmvillaescusa <jmvillaescusa99@gmail.com>
2019-04-05 14:15:52 -0400

    Fight

---
+++ space_escape/space_escape/Fight.cpp
@@ -0,0 +1,5 @@
+#include "Fight.h"
+
+Fight::Fight() {
+
+}; Fight::~Fight() {};
---
+++ space_escape/space_escape/Fight.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+
+class Fight {
+public:
+	Fight();
+	~Fight();
+
+	//void FightManager(BaseStats*, BaseStats*);
+	//void PlayerTurn(BaseStats*, BaseStats*);
+	//void EnemyTurn(BaseStats*, BaseStats*);
+};
