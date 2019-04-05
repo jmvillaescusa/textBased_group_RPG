931649d8e841fc60b15d66fa0d83dbaee81d9706
Ace Dragonair <41645417+acedragonair@users.noreply.github.com>
2019-04-05 14:26:59 -0400

    BASESTAT

---
+++ space_escape/space_escape/Basestat.h
@@ -0,0 +1,42 @@
+#pragma once
+#ifndef BASESTAT_H
+#define BASESTAT_H
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class BaseStatClass {
+public: 
+	unsigned int health;
+	int speed;
+	int power;
+	int luck;
+	int deffence;
+
+public:
+
+	BaseStatClass();
+	~BaseStatClass();
+
+	int getHealth() { return health; }
+	void setHealth(unsigned int m_health) { health = m_health; }
+
+	int getPower() { return power; }
+	void setPower(int m_power) { power = m_power; }
+
+	int getDeffence() { return deffence; }
+	void setDeffence(int m_deffence) { deffence = m_deffence; }
+
+	int getSpeed() { return speed; }
+	void setSpeed(int m_speed) { speed = m_speed; }
+
+	int getLuck() { return luck; }
+	void setLuck(int m_luck) { luck = m_luck; }
+};
+
+#endif // !BASESTAT_H
