242d2ef864152be5175ff6169595a8f949ee1fc9
Frostiblue <46687289+Frostiblue@users.noreply.github.com>
2019-04-05 14:21:34 -0400

    1st

---
+++ space_escape/space_escape/Items.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+
+	const int MAX_ITEMS = 7;
+	string inventory[MAX_ITEMS];
+
+	int numItems = 0;
+	inventory[numItems++] = "Rations";
+	inventory[numItems++] = "Light Shiv";
+	inventory[numItems++] = "Ragged Clothes";
+	inventory[numItems++] = "Pew pew rifle";
+
+
+
+	return 0;
+}
+
