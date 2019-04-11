8fa3bedfbc41c37a6739535ef516f3cfa85e7032
Dylanramper <44001627+Dylanramper@users.noreply.github.com>
2019-04-11 16:43:39 -0400

    tried to add sound

--- space_escape/space_escape/space_escape.cpp
+++ space_escape/space_escape/space_escape.cpp
@@ -4,18 +4,24 @@
 #include "Enemy.h"
 #include "Guards.h"
 #include <iostream>
-
-
-
+#include <Windows.h>
+#include <mmsystem.h>
 
 int main()
 {
+	
+	/*mciSendString("(open\"Halo_2.mp3\"mp3", NULL, 0, NULL);
+	mciSendString("play mp3", NULL, 0, NULL);
+	mciSendString("play mp3 wait", NULL, 0, NULL);*/
 	Player player;
 	bool isRunning = true;
 	int playerPosY = 1;
 	GuardClass Guard;
 	Map m;
 	m.DislayMap();
+
+	//PlaySound(TEXT("Halo_2.mp3"), NULL, SND_FILENAME | SND_ASYNC);
+	//PlaySound(TEXT("Halo_2.mp3"), NULL, 0, NULL);
 
 	player.Update();
 	//player.getPlayerInfo;
